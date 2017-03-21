dbc71a7ff4bf2868f5dc22c76a732f7fd854daa1
Gerald Scott <osullivan.gerald@gmail.com>
2017-03-21 14:32:40 +0200

    Remove hashPrefix.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,36 +1,37 @@
 'use strict';
 
 /**
- * @ngdoc overview
- * @name trcdb2App
- * @description
- * # trcdb2App
- *
- * Main module of the application.
- */
+* @ngdoc overview
+* @name trcdb2App
+* @description
+* # trcdb2App
+*
+* Main module of the application.
+*/
 angular
-  .module('trcdb2App', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+.module('trcdb2App', [
+   'ngAnimate',
+   'ngAria',
+   'ngCookies',
+   'ngMessages',
+   'ngResource',
+   'ngRoute',
+   'ngSanitize'
+])
+.config(function ($routeProvider, $locationProvider) {
+   $locationProvider.hashPrefix(''); // AngularJS 1.6 set hash prefix of '!' by default, so remove it.
+   $routeProvider
+   .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+      controllerAs: 'main'
+   })
+   .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl',
+      controllerAs: 'about'
+   })
+   .otherwise({
+      redirectTo: '/'
+   });
+});
