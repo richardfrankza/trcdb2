a30679b2d19644c3dfd5096abbdc31ded8e948c5
Gerald Scott <osullivan.gerald@gmail.com>
2017-03-21 14:17:11 +0200

    Initial scaffolding by Yeoman.

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name trcdb2App
+ * @description
+ * # trcdb2App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('trcdb2App', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trcdb2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the trcdb2App
+ */
+angular.module('trcdb2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
